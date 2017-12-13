a5a7c6e2c5c3297ff66cb41eb73405692faf6f5e
瑞基 伊藤 <150396@jc-21.jp>
2017-12-13 16:05:33 +0900

    ボタンとかラベルとかを追加したよ

--- URL_encoding_ver5/URL_encoding_ver5/MainPage.xaml.cs
+++ URL_encoding_ver5/URL_encoding_ver5/MainPage.xaml.cs
@@ -9,12 +9,18 @@ namespace URL_encoding_ver5
 {
     public partial class MainPage : ContentPage
     {
+
+        public string Rakuten_URL = "https://app.rakuten.co.jp/services/api/BooksGenre/Search/20121128?format=json|&applicationId=1008026300680682252";
+
+
         public MainPage()
         {
             InitializeComponent();
+            
+        }
 
-
-
+        private void Button_Clicked(object sender, EventArgs e)
+        {
 
         }
     }
